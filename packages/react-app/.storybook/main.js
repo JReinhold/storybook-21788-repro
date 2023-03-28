92536ff76142d732e5dedc5715da091ce18660eb
Jeppe Reinhold <jeppe@reinhold.is>
2023-03-28 22:59:23 +0200

    initial commit

---
+++ packages/react-app/.storybook/main.js
@@ -0,0 +1,25 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+	stories: [
+		"../src/**/*.mdx",
+		"../src/**/*.stories.@(js|jsx|ts|tsx)",
+		"../../vanilla-app/common-stories/**/*.mdx",
+		"../../vanilla-app/common-stories/**/*.stories.@(js|jsx|ts|tsx)",
+	],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+	features: {
+		storyStoreV7: false,
+	},
+	docs: {
+		autodocs: "tag",
+	},
+};
+export default config;
