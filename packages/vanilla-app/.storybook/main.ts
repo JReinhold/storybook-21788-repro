5d70351dc8e856f3c4daa96311e3bb83ee7287ba
Jeppe Reinhold <jeppe@reinhold.is>
2023-03-30 08:56:40 +0200

    initialize projects with ts

---
+++ packages/vanilla-app/.storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/html-vite";
+const config: StorybookConfig = {
+	stories: [
+		"../src/stories/**/*.mdx",
+		"../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+		"../common-stories/**/*.mdx",
+		"../common-stories/**/*.stories.@(js|jsx|ts|tsx)",
+	],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+	],
+	framework: {
+		name: "@storybook/html-vite",
+		options: {},
+	},
+	docs: {
+		autodocs: "tag",
+	},
+};
+export default config;
