bdae0c8bcd69525f7c27371aac33c381d3128f83
Jeppe Reinhold <jeppe@reinhold.is>
2023-09-04 15:25:01 +0200

    upgrade to 7.5.0-alpha.1

--- packages/sveltekit-app/.storybook/main.js
+++ packages/sveltekit-app/.storybook/main.js
@@ -1,3 +1,4 @@
+import { dirname, join } from "path";
 /** @type { import('@storybook/sveltekit').StorybookConfig } */
 const config = {
 	stories: [
@@ -7,9 +8,9 @@ const config = {
 		"../../vanilla-app/common-stories/**/*.stories.@(js|jsx|ts|tsx)",
 	],
 	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions",
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@storybook/addon-interactions"),
 	],
 	framework: {
 		name: "@storybook/sveltekit",
@@ -23,3 +24,7 @@ const config = {
 	},
 };
 export default config;
+
+function getAbsolutePath(value) {
+    return dirname(require.resolve(join(value, "package.json")));
+}
--- packages/vue-app/.storybook/main.js
+++ packages/vue-app/.storybook/main.js
@@ -1,3 +1,4 @@
+import { dirname, join } from "path";
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
 	stories: [
@@ -7,12 +8,12 @@ const config = {
 		"../../vanilla-app/common-stories/**/*.stories.@(js|jsx|ts|tsx)",
 	],
 	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions",
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@storybook/addon-interactions"),
 	],
 	framework: {
-		name: "@storybook/vue3-vite",
+		name: getAbsolutePath("@storybook/vue3-vite"),
 		options: {},
 	},
 	features: {
@@ -23,3 +24,7 @@ const config = {
 	},
 };
 export default config;
+
+function getAbsolutePath(value) {
+    return dirname(require.resolve(join(value, "package.json")));
+}
